9a5ec71a0edca2262cad7e45bbdee643b316de22
Suzana <suzanasouza.caetano@gmail.com>
2024-12-03 10:29:46 -0300

    add rote Put /pessoas/ rota que edita itens

--- Rotas/PessoaRotas.cs
+++ Rotas/PessoaRotas.cs
@@ -9,33 +9,36 @@ namespace apiDotNet.Rotas
         {
             app.MapGet(pattern: "/pessoas", handler: () =>  Pessoas );
 
-            app.MapGet(pattern: "/pessoas/{nome}", handler: (string nome) =>
-            {
-                return Pessoas.Find(x => x.Nome == nome);
-            });
-
             app.MapPost(pattern: "/pessoas", handler: (Pessoa pessoa) =>
             {
-                if (pessoa.Nome.Equals("suzana", StringComparison.OrdinalIgnoreCase));
+                if (pessoa.Nome.Equals("suzana", StringComparison.OrdinalIgnoreCase))
                 {
                     return Results.BadRequest(error: new { message = "Erro pos a adm é a suzana" });
                 }
-                
+                else
                 {
                     Pessoas.Add(pessoa);
                     return Results.Ok(pessoa);
                 }
-            }
+            });
+
+            app.MapPut(pattern: "/pessoas/{id}", handler: (Guid id, Pessoa pessoa) =>
+            {
+                Pessoa? encontrado = Pessoas.Find(x => x.Id == id);
+                if (encontrado == null)
+                    return Results.NotFound();
+                else
+                    encontrado.Nome = pessoa.Nome;
+                return Results.Ok(encontrado);
             });
         }
 
-        public static  List<Pessoa> Pessoas = new()
+        public static List<Pessoa> Pessoas = new()
         {
             new Pessoa(Guid.NewGuid(), nome:"Freddie Mercury"),
             new Pessoa(Guid.NewGuid(), nome:"Jean Poul Sartre"),
             new Pessoa(Guid.NewGuid(), nome:"Sêneca"),
-            new Pessoa(Guid.NewGuid(), nome:"Marco Aurélio"),
+            new Pessoa(Guid.NewGuid(), nome:"Marco Aurélio")
         };
-       
     }
 }
